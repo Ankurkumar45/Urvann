4688c5bd562e9b68180f7f71a091dae9440307e8
Ankurkumar45 <ankurkumardwivedi779@gmail.com>
2025-08-25 14:56:01 +0530

    Fix CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,16 @@ dotenv.config();
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://urvann-yo9v.vercel.app'
+    'https://urvann-yo9v.vercel.app',
+    'https://urvann-mini-plant-store.onrender.com'
 ]
 
 app.use(cors(
     {
         origin: allowedOrigins,
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE']
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
     }
 ));
 app.use(express.json());
